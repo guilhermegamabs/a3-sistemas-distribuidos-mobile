add8a351ef609475ceab6c92440d4430c9849373
Guilherme Gama <guilhermegamabs@gmail.com>
2024-05-04 12:59:30 -0300

    initial do server side

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,4 +1,38 @@
 package server;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        int portServer = 5509;
+
+        Scanner scanner = new Scanner(System.in);
+
+        String inputData;
+
+        System.out.printf("Informe a PORTA do Servidor (PORTA DEFAULT: %s): ", portServer);
+        inputData = scanner.nextLine();
+
+        portServer = inputData.length() > 0 ? Integer.parseInt(inputData) : portServer;
+
+        try {
+            serverSocket = new ServerSocket(portServer);
+
+            while (true) {
+                System.out.println("Aguardando o player...");
+                Socket player = serverSocket.accept();
+                System.out.println("Conectado com o " + player);
+
+                
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR on Server side " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+
+    }
 }
