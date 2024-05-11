ddd9588a653029d9adf728db351689011ded6659
Guilherme Gama <guilhermegamabs@gmail.com>
2024-05-11 14:52:11 -0300

    arrumando para conseguir salvar e atualizar o número total do scoreboard

--- src/server/PlayMatch.java
+++ src/server/PlayMatch.java
@@ -27,30 +27,32 @@ public class PlayMatch extends Thread {
     private void playSingle() {
         Random random = new Random();
         Response response = null;
+        Player player1 = new Player();
+
         int opcao = 0;
 
         try {
-            Message messagePlayer1 = null;
-
             while(opcao != 4) {
                 Message messageServer = new Message("Servidor", random.nextInt(3) + 1);
-                messagePlayer1 = (Message) communicationPlayer1.receive();
+                Message messagePlayer1 = (Message) communicationPlayer1.receive();
                     if(messagePlayer1.getOpcao() != 4) {
-                        System.out.println("Jogador - " + messagePlayer1.getOpcao() + " X " + messageServer.getOpcao() + " - Server");
+                        System.out.println(messagePlayer1.getPlayerName() + " - " + messagePlayer1.getOpcao() + " X " + messageServer.getOpcao() + " - Server");
                     }
 
                     if (messagePlayer1.getOpcao() == messageServer.getOpcao()) {
                         messagePlayer1.empatou();
-                        response = new Response(messagePlayer1.getPlayerName(), messagePlayer1.getVitorias(), messagePlayer1.getEmpates(), messagePlayer1.getDerrotas());
+                        player1.increaseDraws();
                     } else if (messagePlayer1.getOpcao() == 1 && messageServer.getOpcao() == 2 || messagePlayer1.getOpcao() == 2 && messageServer.getOpcao() == 3 || messagePlayer1.getOpcao() == 3 && messageServer.getOpcao() == 1 ) {
                         messagePlayer1.venceu();
-                        response = new Response(messagePlayer1.getPlayerName(), messagePlayer1.getVitorias(), messagePlayer1.getEmpates(), messagePlayer1.getDerrotas());
+                        player1.increaseVictories();
                     } else if(messagePlayer1.getOpcao() == 2 && messageServer.getOpcao() == 1 || messagePlayer1.getOpcao() == 3 && messageServer.getOpcao() == 2 || messagePlayer1.getOpcao() == 1 && messageServer.getOpcao() == 3 ){
                         messagePlayer1.perdeu();
-                        response = new Response(messagePlayer1.getPlayerName(), messagePlayer1.getVitorias(), messagePlayer1.getEmpates(), messagePlayer1.getDerrotas());
+                        player1.increaseDefeats();
                     } else {
                         opcao = 4;
                     }
+
+                    response = new Response(messagePlayer1.getPlayerName(), player1.getVictories(), player1.getDraws(), player1.getDefeats());
 
                 communicationPlayer1.send(response);
                 }
